9825e3207361599b2b2b12c2542369a95de56da1
idan100 <31824300+idan100@users.noreply.github.com>
2023-09-12 13:06:06 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ resource "azurerm_virtual_machine" "main" {
   name                  = "super-basic-vm"
   location              = "westeurope"
   resource_group_name   = "raamses-gaia-playground"
-  network_interface_ids = [azurerm_network_interface.main.id]
+  network_interface_ids = [azurerm_network_interface.nic1.id]
   vm_size               = "Standard_DS1_v2"
 }
