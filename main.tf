41cd7f5604c9c49e03c19a9e36f2b2bd5a6f81a2
idan100 <31824300+idan100@users.noreply.github.com>
2023-09-12 14:51:49 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,21 +11,21 @@ resource "azurerm_resource_group" "example" {
 resource "azurerm_virtual_network" "example" {
   name                = "example-network"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  resource_group_name = "raamses-gaia-playground"
+  location            = "westeurope"
 }
 
 resource "azurerm_subnet" "example" {
   name                 = "internal"
-  resource_group_name  = azurerm_resource_group.example.name
+  resource_group_name = "raamses-gaia-playground"
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_network_interface" "example" {
   name                = "example-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  resource_group_name = "raamses-gaia-playground"
+  location            = "westeurope"
 
   ip_configuration {
     name                          = "internal"
